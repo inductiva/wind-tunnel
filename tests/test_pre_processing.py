83ff3ea95a5a4c9c8c2d5624019bbd05ab6f1d88
rcvalerio <rvalerio@inductiva.ai>
2024-08-01 12:15:13 +0100

    Added preprocessing tests

---
+++ tests/test_pre_processing.py
@@ -0,0 +1,68 @@
+"""Unit tests of the pre_processing submodule."""
+import math
+
+import pytest
+import pyvista as pv
+
+import windtunnel
+
+RESOLUTION = 1000
+
+
+@pytest.mark.parametrize(
+    "radius",
+    [1, 5, 30],
+)
+def test_compute_projected_area_sphere(radius):
+    sphere = pv.Sphere(
+        radius=radius,
+        theta_resolution=RESOLUTION,
+        phi_resolution=RESOLUTION,
+    )
+
+    expected_area = math.pi * radius**2
+    computed_area = windtunnel.pre_processing.compute_projected_area(
+        sphere,
+        face_normal=(1, 0, 0),
+    )
+    assert computed_area == pytest.approx(expected_area, rel=1e-2)
+
+
+@pytest.mark.parametrize(
+    "radius,height",
+    [(10, 20), (10, 100), (30, 10), (9999, 10)],
+)
+def test_compute_projected_area_cylinder_top(radius, height):
+    cylinder = pv.Cylinder(
+        radius=radius,
+        height=height,
+        resolution=RESOLUTION,
+    )
+
+    expected_area = math.pi * radius**2
+    computed_area = windtunnel.pre_processing.compute_projected_area(
+        cylinder,
+        face_normal=(1, 0, 0),
+    )
+
+    assert computed_area == pytest.approx(expected_area, rel=1e-2)
+
+
+@pytest.mark.parametrize(
+    "radius,height",
+    [(10, 20), (10, 100), (30, 10), (9999, 10)],
+)
+def test_compute_projected_area_cylinder_side(radius, height):
+    cylinder = pv.Cylinder(
+        radius=radius,
+        height=height,
+        resolution=RESOLUTION,
+    )
+
+    expected_area = 2 * radius * height
+    computed_area = windtunnel.pre_processing.compute_projected_area(
+        cylinder,
+        face_normal=(0, 1, 0),
+    )
+
+    assert computed_area == pytest.approx(expected_area, rel=1e-2)
